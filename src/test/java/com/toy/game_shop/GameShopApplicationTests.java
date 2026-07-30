f58da9b317eb80048f798711effe60a747292ac2
shay0454 <shay0454@naver.com>
2026-07-30 14:51:34 +0900

    Initial commit

---
+++ src/test/java/com/toy/game_shop/GameShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.game_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
