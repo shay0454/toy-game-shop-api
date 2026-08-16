7fdbdc9b0fd1aa349ca1b90170ae6b846d5d4c14
shay0454 <shay0454@naver.com>
2026-08-16 21:55:41 +0900

    fix: ItemCreateRequest.type의 잘못된 @NotNull 제거
    type 미입력 시 NONE으로 기본값 처리하는 ItemService.addItem()의
    기존 로직이, DTO 검증 도입 과정에서 실수로 붙은 @NotNull 때문에
    컨트롤러 단계에서 400으로 막혀 도달 불가능해진 문제 수정

--- src/main/java/com/toy/game_shop/dto/item/ItemCreateRequest.java
+++ src/main/java/com/toy/game_shop/dto/item/ItemCreateRequest.java
@@ -3,7 +3,6 @@ package com.toy.game_shop.dto.item;
 import com.toy.game_shop.type.ItemType;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +13,6 @@ public class ItemCreateRequest{
     @NotBlank(message = "Item명 입력")
     private String name;
 
-    @NotNull
     private ItemType type;
 
     @Size(max = 500)
