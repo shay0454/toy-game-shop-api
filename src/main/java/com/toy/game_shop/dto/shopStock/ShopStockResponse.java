f52081485ba92a1025f432cbbf779e43ea340397
shay0454 <shay0454@naver.com>
2026-08-18 19:45:42 +0900

    refactor: Transaction/ShopStock 응답 평탄화 (참조 엔티티 → id+name)
    - TransactionResponse: player/item/shop 전체 객체 대신
      playerId/itemId/shopId + nickname/itemName/shopName으로 평탄화
    - ShopStockResponse: shop/item 전체 객체 대신
      shopId/itemId + shopName/itemName으로 평탄화

--- src/main/java/com/toy/game_shop/dto/shopStock/ShopStockResponse.java
+++ src/main/java/com/toy/game_shop/dto/shopStock/ShopStockResponse.java
@@ -1,24 +1,26 @@
 package com.toy.game_shop.dto.shopStock;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.toy.game_shop.dto.item.ItemResponse;
-import com.toy.game_shop.dto.shop.ShopResponse;
 import com.toy.game_shop.entity.ShopStock;
 import lombok.Getter;
 
 @Getter
-@JsonPropertyOrder({"id","shop","item","stock","price"})
+@JsonPropertyOrder({"id","shopId","itemId","shopName","itemName","stock","price"})
 public class ShopStockResponse {
-    private Long id;
-    private ShopResponse shop;
-    private ItemResponse item;
-    private Integer stock;
-    private Long price;
+    private final Long id;
+    private final Long shopId;
+    private final Long itemId;
+    private final String shopName;
+    private final String itemName;
+    private final Integer stock;
+    private final Long price;
 
     public ShopStockResponse(ShopStock stock){
         this.id = stock.getId();
-        this.shop = new ShopResponse(stock.getShop());
-        this.item = new ItemResponse(stock.getItem());
+        this.shopId = stock.getShop().getId();
+        this.itemId = stock.getItem().getId();
+        this.shopName = stock.getShop().getName();
+        this.itemName = stock.getItem().getName();
         this.stock = stock.getStock();
         this.price = stock.getPrice();
     }
--- src/main/java/com/toy/game_shop/dto/transaction/TransactionResponse.java
+++ src/main/java/com/toy/game_shop/dto/transaction/TransactionResponse.java
@@ -1,30 +1,33 @@
 package com.toy.game_shop.dto.transaction;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.toy.game_shop.dto.item.ItemResponse;
-import com.toy.game_shop.dto.player.PlayerResponse;
-import com.toy.game_shop.dto.shop.ShopResponse;
 import com.toy.game_shop.entity.Transaction;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@JsonPropertyOrder({"id","player","item","shop","quantity","priceAtTransaction","transactedTime"})
+@JsonPropertyOrder({"id","playerId","itemId","shopId","nickname","itemName","shopName","quantity","priceAtTransaction","transactedTime"})
 public class TransactionResponse {
     private final Long id;
-    private final PlayerResponse player;
-    private final ItemResponse item;
-    private final ShopResponse shop;
+    private final Long playerId;
+    private final Long itemId;
+    private final Long shopId;
+    private final String nickname;
+    private final String itemName;
+    private final String shopName;
     private final Integer quantity;
     private final Long priceAtTransaction;
     private final LocalDateTime transactedTime;
 
     public TransactionResponse(Transaction transaction){
         this.id = transaction.getId();
-        this.player = new PlayerResponse(transaction.getPlayer());
-        this.item = new ItemResponse(transaction.getItem());
-        this.shop = new ShopResponse(transaction.getShop());
+        this.playerId = transaction.getPlayer().getId();
+        this.itemId = transaction.getItem().getId();
+        this.shopId = transaction.getShop().getId();
+        this.nickname = transaction.getPlayer().getNickname();
+        this.itemName = transaction.getItem().getName();
+        this.shopName = transaction.getShop().getName();
         this.quantity = transaction.getQuantity();
         this.priceAtTransaction = transaction.getPriceAtTransaction();
         this.transactedTime = transaction.getTransactedTime();
